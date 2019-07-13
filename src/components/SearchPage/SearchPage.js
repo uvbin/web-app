dfabc074314a300dc485d2dd437912c0b011e7ca
adrianjustinmartinez@gmail.com <adrianjustinmartinez@gmail.com>
2019-07-13 11:40:30 -0700

    Minor fix

--- src/components/SearchPage/SearchPage.js
+++ src/components/SearchPage/SearchPage.js
@@ -1,22 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Banner from '../Banner/Banner';
 import ContentSearch from '../ContentSearch/ContentSearch';
 
 import ContentViewer from '../ContentViewer/ContentViewer';
 
 
-class SearchPage extends Component {
-
-    render () {
-        return (
-            <div>
-                <Banner>
-                    <ContentSearch />
-                </Banner>
-                <ContentViewer />
-            </div>
-        );
-    }
+const searchPage = () => {
+    return (
+        <div>
+            <Banner>
+                <ContentSearch />
+            </Banner>
+            <ContentViewer />
+        </div>
+    );
 }
 
-export default SearchPage;
+export default searchPage;
