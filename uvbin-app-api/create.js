21a81608891cd297e4db10a77dc9762d02174826
Clint Blatchford <clintblatchford@gmail.com>
2019-07-17 00:11:47 -0700

    CRUD api's created, serverless deployment to aws

---
+++ uvbin-app-api/create.js
@@ -0,0 +1,25 @@
+import uuid from "uuid";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: "models",
+    Item: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      modelId: uuid.v1(),
+      content: data.content,
+      attachment: data.attachment,
+      categories: data.categories,
+      createdAt: Date.now()
+    }
+  };
+
+  try {
+    await dynamoDbLib.call("put", params);
+    return success(params.Item);
+  } catch (e) {
+    return failure({ status: false });
+  }
+}
