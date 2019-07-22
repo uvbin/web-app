3bbfadd2cf843e746dfafd1a80633e093d01cc17
Clint Blatchford <clintblatchford@gmail.com>
2019-07-21 23:13:45 -0700

    added routes. see comments in app.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Route, Switch } from "react-router-dom";
+// import of components associated with a path
 import Layout from './containers/Layout/Layout';
+import AccountPage from './components/AccountPage/AccountPage';
+
 
 function App() {
   return (
-    <Layout />
+    // Switch controls which component is loaded based on the path
+    <Switch>
+      {/* Route defines the component associated with the path
+      In this case we are defining the paths to be exact.
+      If we were to leave off "exact" the first path that matches
+      the address in the browser would be loaded. Order matters Ex. "/" 
+      would always load since it is first character in every address.
+      The way I set it up this won't happen. localhost:3000 always loads
+      Layout. .../accountPage always loads AccountPage. I put / last 
+      without exact so that anything not matching our exact routes will
+      get picked up there and Layout will load: localhost:3000/asdfasd 
+      will load Layout.*/}
+      <Route exact path="/" component={Layout} />
+      <Route exact path="/layout" component={Layout} />
+      <Route exact path="/accountPage" component={AccountPage} />
+      <Route path="/" component={Layout} />
+    </Switch>
   );
 }
 
---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-west-1",
+      BUCKET: "uvbin-model-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://zl92weitsf.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_EJzLZKxIy",
+      APP_CLIENT_ID: "297uoavaro3prc9ulu88mu5mbe",
+      IDENTITY_POOL_ID: "us-east-2:aa0e6b8d-ca30-4b17-9383-38c0bc8ef060"
+    }
+  };
