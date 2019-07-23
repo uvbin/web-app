555d5625d2ba9d5b8cc751a917f3726619f9022c
Adrian Martinez <adrianjustinmartinez@gmail.com>
2019-07-23 07:49:34 -0700

    Reactstrap installed, see wiki for details

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 //import config from "./config";
 import './index.css';
+// Necessary for reactstrap to work
+import 'bootstrap/dist/css/bootstrap.css';
 
 // For use later - hitting backend
 // Amplify.configure({
