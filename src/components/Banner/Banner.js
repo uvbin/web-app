0cf4a9ee364677b84d70746bc93a8c55469f1f26
Clint Blatchford <clintblatchford@gmail.com>
2019-08-01 22:14:41 -0700

    initial refactor using material-ui

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,17 +1,61 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import classes from './Banner.module.css';
 import Aux from '../../hoc/Auxiliary';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
 import BannerImage from '../../assets/SearchPage/BannerImage.jpg';
 
-const banner = (props) => {
+
+const ElevationScroll = (props) => {
+    const { 
+        children 
+    } = props;
+
+    const trigger = useScrollTrigger({
+      disableHysteresis: true,
+      threshold: 0,
+    });
+  
+    return React.cloneElement(children, {
+      elevation: trigger ? 4 : 0,
+    });
+  }
+
+const Banner = (props) => {
+    //body
+
     return (
-        <div className={classes.container}>
-            <img src={BannerImage} className={classes.BannerImage}/>
-            <div className={classes.centered}>
-                {props.children}
-            </div>
-        </div>
+        <React.Fragment>
+        <CssBaseline />
+        <ElevationScroll {...props}>
+          <AppBar>
+            <Toolbar>
+              <Typography variant="h6">uvbin</Typography>
+            </Toolbar>
+          </AppBar>
+        </ElevationScroll>
+        <Toolbar />
+        <Container>
+          <Box my={2}>
+            {[...new Array(12)]
+              .map(
+                () => `Cras mattis consectetur purus sit amet fermentum.
+  Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+  Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+  Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
+              )
+              .join('\n')}
+          </Box>
+        </Container>
+      </React.Fragment>
     );
 }
 
-export default banner;
+export default Banner;
--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,16 +1,22 @@
 import React, { useState } from 'react';
-import Aux from '../../hoc/Auxiliary';
 import SearchPage from '../../components/SearchPage/SearchPage';
 import AccountPage from '../../components/AccountPage/AccountPage.js';
-import classes from './Layout.module.css';
 import {
     Layout as PageLayout,
     Menu,
     Breadcrumb,
 } from 'antd';
+import classes from './Layout.module.css';
 
 const { SubMenu } = Menu;
 const { Header, Content, Sider } = PageLayout;
+
+const headerStyle = {
+    lineHeight: '48px',
+    height: '48px',
+    display: 'flex',
+    padding: '0px',
+};
 
 
 const Layout = (props) => {
@@ -25,18 +31,20 @@ const Layout = (props) => {
 
     return (
         <PageLayout>
-            <Header className={classes.header}>
-                {/* <div>
-                    <a href="/" className={classes.logo}>uvbin</a>
-                </div> */}
+            <Header style={headerStyle}>
+                <div className={classes.headerLeft}>
+                    <a className={classes.logo}>uvbin</a>
+                </div>
+                <div className={classes.headerMiddle}></div>
                 <Menu 
+                    className={[classes.menu, classes.headerRight].join(' ')}
                     theme="dark"
                     mode="horizontal"
-                    defaultSelectedKeys={['2']}
-                    style={{ lineHeight: '64px' }}>
+                    defaultSelectedKeys={['2']}>
                     <Menu.Item key="account">My Account</Menu.Item>
                     <Menu.Item key="contact">Contact</Menu.Item>
                     <Menu.Item key="about">About</Menu.Item>
+
                 </Menu>
             </Header>
             {state.currentPage}
